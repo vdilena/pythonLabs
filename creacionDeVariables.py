db3a0e8731abf3467743992033d3ca29ec34e023
Victor Di Lena <victor.dilena@rcpo.com.ar>
2024-09-22 12:43:15 -0300

    Primeros pasos en python

---
+++ creacionDeVariables.py
@@ -0,0 +1,11 @@
+
+variableNumerica = 1234 # Tipo int
+variableDecimal = 12.34 # Tipo float
+variableNumeroComplejo = 2 + 3j # Tipo complex
+variableNula = None # Tipo None
+variableString = "Esta es una variable de tipo String" # Tipo string
+variableBooleana = True
+
+print(variableNumerica)
+print(variableString)
+print(variableBooleana)
