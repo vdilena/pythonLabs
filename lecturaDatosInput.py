c569170104bc06f5a237f1b8cb6402c1c1b4f88e
Victor Di Lena <victor.dilena@rcpo.com.ar>
2024-10-04 19:25:51 -0300

    Script de lectura de datos de usuario con input

---
+++ lecturaDatosInput.py
@@ -0,0 +1,6 @@
+# Leo datos del usaurio con la funcion input
+nombre = input("Cual es su nombre? ")
+apellido = input("Y tu apellido? ")
+
+# Imprimo por pantalla con f-string
+print(f'Nombre {nombre}, Apellido: {apellido}')
