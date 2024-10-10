43f1e176a01b51aafe3bf8afec08070cba7e14a5
Victor Di Lena <victor.dilena@rcpo.com.ar>
2024-10-10 10:33:09 -0300

    Se muestra un ejemplo de desempaquetado de tuplas

--- tuplas.py
+++ tuplas.py
@@ -5,4 +5,7 @@ print(tuplaNumeros)
 print(tuplaPaises)
 print(tuplaPaises[2]) # Accedo al tercer elemento de la tupla
 print(len(tuplaPaises)) # Obtengo la longitud de la tupla
+# Hago un unpack de la tupla de paieses
+(primero, segundo, tercero, cuarto) = listaPaises
+print(f'Primer pais: {primero}, segundo pais: {segundo}, tercer pais: {tercero}')
 tuplaNumeros[1] = 10 # Esto deberia dar error, porque las tuplas son inmutables
