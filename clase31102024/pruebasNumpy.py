382ea72a7dea1f26246412beb3b12f6a575ecd21
Victor Di Lena <victor.dilena@rcpo.com.ar>
2024-10-30 20:36:20 -0300

    Mejoro descripciones

---
+++ clase31102024/pruebasNumpy.py
@@ -0,0 +1,75 @@
+
+
+""" Lista original """
+
+# Lista de 500000 numeros aleatorios del 1 al 500
+
+# Mido el tiempo que tarda en recorrer la lista
+
+# Transformo una lista de una dimension en un array
+
+# Mido el tiempo que tarda en recorrer el array de una dimension
+
+
+""" Matriz original """
+
+# Transformo una lista de 2 dimensiones en un array tipo matriz
+
+# Quiero saber las dimensiones de los arrays
+
+# Quiero saber las estructuras de los arrays
+
+# Quiero saber los tipos de datos de los arrays
+
+# Quiero saber la cantidad de datos de los arrays
+
+# Quiero crear una matriz inicializada con ceros de 5 x 4
+
+# Quiero crear una matriz con una secuencia del 0 al 16
+
+# Quiero crear un array con una secuencia del 5 al 50, de 5 en 5
+
+# Quiero crear un array con 20 numeros consecutivos distribuidos entre el 1 y el 100
+
+# Quiero graficar como se distribuye el seno, coseno y tangente de un circulo y ver el grafico para 20 valores consecutivos entre 0 y 2π
+
+
+""" Operaciones con arrays """
+
+# Quiero concatenar arreglos
+
+# Quiero ordenar uno de los arrays anteriores de forma descendente
+
+# Quiero generar 500 numeros aleatorios entre 0 y 1, con una semilla de 2 bits para la reproducibilidad
+
+# Quiero que me haga un calculo de una normal con los numeros aleatorios con una media de 5 y una desviacion estandar de 2 para 2000 numeros
+
+""" Operaciones estadisticas con arrays """
+
+# Cargo 200 numeros enteros del 0 al 50
+
+# Obtengo el minimo del array numerosAleatoriosEnteros
+
+# Obtengo el maximo del array numerosAleatoriosEnteros
+
+# Obtengo el promedio del array numerosAleatoriosEnteros
+
+# Obtengo la desviacion estandar de los elementos del array numerosAleatoriosEnteros
+
+# Obtengo la sumatoria de los elementos del array numerosAleatoriosEnteros
+
+# Cargo 2000 numeros enteros del 0 al 100 en una matriz de 6x6
+
+# Obtengo el promedio de matrizNumerosAleatoriosEnteros de toda la matriz
+
+# Obtengo el minimo de las columnas en matrizNumerosAleatoriosEnteros
+
+# Obtengo el maximo de las filas en matrizNumerosAleatoriosEnteros
+
+# Obtengo todos los numeros de la matriz mayores a 35
+
+# Quiero acceder a la posicion fila 3, columa 5 de la matriz
+
+# Quiero acceder a los elementos de la tercer columna de la fila 2 a la fila 5
+
+# Quiero acceder a los elementos de la primera y segunda columna de la fila 1 a la fila 4
